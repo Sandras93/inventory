8b1a9b5b5da5dd6adbaaa7476b15c50cc3e02387
Sandras93 <somogyi.andras93@gmail.com>
2023-07-05 12:06:02 +0200

    custserv

--- src/test/java/com/Bander/inventory/customer/CustomerServiceTest.java
+++ src/test/java/com/Bander/inventory/customer/CustomerServiceTest.java
@@ -12,32 +12,33 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class CustomerServiceTest {
     @Autowired
-    CustomerService customerService;
+    private CustomerService customerService;
 
+    CustomerDto customerDto1 = CustomerDto.builder()
+            .customerName("Test Customer")
+            .address("1122, Mucsaröcsöge 1.")
+            .email("test@example.com")
+            .phoneNum("0630-123-4567")
+            .dateOfBirth(LocalDate.parse("2000-01-01"))
+            .customerGender(CustomerGender.MALE)
+            .active(true)
+            .build();
 
-//    CustomerDto customerDto2 = CustomerDto.builder()
-//            .customerName("Bruh Csutomer")
-//            .address("4422, Bruhville 1.")
-//            .email("test@bruhmail.com")
-//            .phoneNum("0630-7654-321")
-//            .dateOfBirth(LocalDate.parse("1900-01-01"))
-//            .customerGender(CustomerGender.FEMALE)
-//            .active(true)
-//            .build();
+    CustomerDto customerDto2 = CustomerDto.builder()
+            .customerName("Bruh Csutomer")
+            .address("4422, Bruhville 1.")
+            .email("test@bruhmail.com")
+            .phoneNum("0630-7654-321")
+            .dateOfBirth(LocalDate.parse("1900-01-01"))
+            .customerGender(CustomerGender.FEMALE)
+            .active(true)
+            .build();
 
 
     @Test
     void createCustomerTest() {
 
-        CustomerDto customerDto1 = CustomerDto.builder()
-                .customerName("Test Customer")
-                .address("1122, Mucsaröcsöge 1.")
-                .email("test@example.com")
-                .phoneNum("0630-123-4567")
-                .dateOfBirth(LocalDate.parse("2000-01-01"))
-                .customerGender(CustomerGender.MALE)
-                .active(true)
-                .build();
+
 
     CustomerDto savedCustomerDto = customerService.createCustomer(customerDto1);
 
